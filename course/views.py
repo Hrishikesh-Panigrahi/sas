211146dc248ba69aa5c57b339386d233f5a0578f
sd8capricon <sd8capricon@gmail.com>
2022-06-06 14:17:48 +0530

    course model

--- course/views.py
+++ course/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+from django.views import View
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class Index(View):
+    def get(self, request):
+        return HttpResponse("hello world")
