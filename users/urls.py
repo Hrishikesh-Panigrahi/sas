788d14ec8834ee9cd7bc25a69f0bcc0f29177487
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2022-06-06 19:00:34 +0530

    minor changes

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('Sign-Up', views.register, name='register'),
-
+    path('Register', views.register, name= 'register'),
 ]
