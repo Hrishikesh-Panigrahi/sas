7d3d8bf9e92380839fcb84329ccae3a180d9d65d
sd8capricon <sd8capricon@gmail.com>
2022-06-07 14:25:13 +0530

    verbose course model

--- course/forms.py
+++ course/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import CheckboxSelectMultiple, ModelForm, ModelMultipleChoiceField
 from course.models import Course
 from users.models import TeacherProfile
 
@@ -10,9 +10,21 @@ class CourseForm(ModelForm):
         #           'dept_level', 'institue_level']
         fields = '__all__'
 
+    # taught_by = ModelMultipleChoiceField(
+    #     queryset=TeacherProfile.objects.all(), widget=CheckboxSelectMultiple)
+
     # add dept as params in CoureForm Object
     # dept should be as per dept_choices
     # def __init__(self, dept, *args, **kwargs):
+    #     super(CourseForm, self).__init__(*args, **kwargs)
+    #     self.fields['dept'].initial = dept
+    #     self.fields['dept'].disabled = True
+    #     self.fields['taught_by'].queryset = TeacherProfile.objects.filter(
+    #         department=dept)
+
+    # For testing ignore
+    # def __init__(self, *args, **kwargs):
+    #     dept = 'Computer Engineering'
     #     super(CourseForm, self).__init__(*args, **kwargs)
     #     self.fields['dept'].initial = dept
     #     self.fields['dept'].disabled = True
