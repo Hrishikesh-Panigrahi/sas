e56d833b41cdd201be346e1a9df2d04d6b486ddb
sd8capricon <sd8capricon@gmail.com>
2022-06-07 15:27:28 +0530

    added dashboard index

--- sas/urls.py
+++ sas/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from sas.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
     path('users/', include('users.urls')),
     path('course/', include('course.urls'))
 ]
