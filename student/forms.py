259688b4bf5be695e4ceee6b6658cfc832d8644e
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2022-06-12 17:23:18 +0530

    CRUD for student

--- student/forms.py
+++ student/forms.py
@@ -4,12 +4,39 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
+class StudentForm(forms.ModelForm):
+    class Meta:
+        # my_form = 'userForm'
+        model = student
+        fields = ['department',
+                  'DOB', 'address']
+        # widgets = {
+        #     'department': forms.Select(attrs={'form': my_form}),
+        #     # 'course': forms.CheckboxSelectMultiple(attrs={'form': my_form}),
+        #     'address': forms.TextInput(attrs={'class': 'form-control', 'form': my_form}),
+        #     'DOB': forms.DateField(attrs={'class': 'form-control', 'form': my_form}),
+        # }
+
+    
+       
+
 class UserForm(UserCreationForm):
-    email = forms.EmailField(label = "Email")
-    first_name = forms.CharField(label = "First name")
-    last_name = forms.CharField(label = "Last name")
-
-    class meta:
+    class Meta:
+        # my_form = 'userForm'
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        
+        fields = ['username', 'first_name', 'last_name']
+        # widgets = {
+        #     'username': forms.TextInput(attrs={'class': 'form-control', 'form': my_form}),
+        #     'first_name': forms.TextInput(attrs={'class': 'form-control', 'form': my_form}),
+        #     'last_name': forms.TextInput(attrs={'class': 'form-control', 'form': my_form}),
+            
+        # }
 
+    # def __init__(self, *args, **kwargs):
+    #     my_form = 'userForm'
+    #     super(UserForm, self).__init__(*args, **kwargs)
+    #     self.fields['password1'].widget = forms.PasswordInput(
+    #         attrs={'class': 'form-control', 'type': 'password', 'form': my_form})
+    #     self.fields['password2'].widget = forms.PasswordInput(
+    #         attrs={'class': 'form-control', 'type': 'password', 'form': my_form})
