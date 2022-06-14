52adbef26b6ac348387603af5554196c7831fd43
sd8capricon <sd8capricon@gmail.com>
2022-06-14 12:49:07 +0530

    student dept choices

--- student/models.py
+++ student/models.py
@@ -1,60 +1,39 @@
 from django.contrib.auth.models import User
 from django.db import models
-from course.models import  Course
+from course.models import Course
 
 # User -> username, first_name, last_name, password, email, is_staff, is_active, is_superuser, date_created
-#         last_login/last_updated 
+#         last_login/last_updated
 
-# auth_session, auth_users, auth_active, 
+# auth_session, auth_users, auth_active,
 
 # Create your models here.
 
+
 class student(models.Model):
-    
-    user = models.OneToOneField(User, on_delete=models.CASCADE)    
+
+    department_choices = [
+        ('Computer Engineering', 'Computer Engineering'),
+        ('Electronics and Telecommunication Engineering',
+         'Electronics and Telecommunication Engineering'),
+        ('Information Technology', 'Information Technology'),
+        ('Mechanical Engineering', 'Mechanical Engineering'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     # name =models.CharField(max_length=30)
     # div = models.CharField(max_length=2)
-    department = models.CharField(max_length=100, null=True)
-    roll_no=models.IntegerField(null = True)
-    DOB=models.DateField(null = True)
-    address=models.TextField(null = True)
+    department = models.CharField(
+        max_length=100, null=True, choices=department_choices)
+    roll_no = models.IntegerField(null=True)
+    DOB = models.DateField(null=True)
+    address = models.TextField(null=True)
     # email= models.EmailField()
     # id = (str(roll_no)+str(DOB))
-    #attendance=something
+    # attendance=something
     id = models.BigAutoField(primary_key=True)
-    course=models.ManyToManyField("course.Course", verbose_name=("Enrolled Course"))
+    course = models.ManyToManyField(
+        "course.Course", verbose_name=("Enrolled Course"))
 
     def __str__(self):
-        return str(self.id) + " " +self.user.username 
-
-
-    
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        
-        
-        
-        
-        
-        
-        
-    
-    
-
-    
-    
-    
-    
-
+        return str(self.id) + " " + self.user.username
