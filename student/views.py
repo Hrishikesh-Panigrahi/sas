438168e064342dc62997bb5fb14de486aefb7ff6
sd8capricon <sd8capricon@gmail.com>
2022-06-16 13:40:04 +0530

    added students title to contexts

--- student/views.py
+++ student/views.py
@@ -13,9 +13,9 @@ def register(request):
     context = {}
     # studentForm=StudentForm(request.POST)
     userform = UserForm(request.POST)
-    course =Course.objects.all()
+    course = Course.objects.all()
     studentForm = StudentForm(courseSet=course)
-    if request.method=='POST':
+    if request.method == 'POST':
         userform = UserForm(request.POST)
         print('1')
         # studentForm = StudentForm(request.POST ,courseSet=course)
@@ -24,7 +24,7 @@ def register(request):
             c_ids = request.POST.getlist('course')
             print('2')
             s = User.objects.get(username=request.POST['username'])
-            stu = student(user = s)
+            stu = student(user=s)
             print(c_ids)
             for i in c_ids:
                 c = Course.objects.get(pk=i)
@@ -32,54 +32,59 @@ def register(request):
                 print('course added saved')
             stu.save()
             print('student saved')
-            
+
             return redirect("/")
-    context = {'userform': userform, 'type' : 'Register', 'sform': studentForm}
-    return render(request, 'student/studentRegister.html ' , context)
+    context = {
+        'userform': userform,
+        'type': 'Register',
+        'sform': studentForm,
+        'title': 'Students'
+    }
+    return render(request, 'student/studentRegister.html ', context)
 
 
 def studentList(request):
-    
-    
+
     stu = student.objects.all()
-    
-    
-    context = {'student' : stu}
-    if stu.count()==0:
-        return render(request,'student/emptystudent.html')
+
+    context = {
+        'student': stu,
+        'title': 'Students'
+    }
+    if stu.count() == 0:
+        return render(request, 'student/emptystudent.html')
     else:
         return render(request, 'student/Studentlist.html', context)
-        
-        
-
-   
 
 
 def delete(request, pk):
-    stu = student.objects.get(id = pk)
+    stu = student.objects.get(id=pk)
     context = {}
     if request.method == 'POST':
         user = stu.user
         stu.delete()
         user.delete()
         return redirect("/")
-            
-    context = {'id': stu.id, 'type' : 'delete'}
 
-    return render(request, 'student/delete.html ' , context)
+    context = {
+        'id': stu.id,
+        'type': 'delete',
+        'title': 'Students'
+    }
 
+    return render(request, 'student/delete.html ', context)
 
 
 def update(request, pk):
     course = Course.objects.all()
-    stu = student.objects.get(id = pk)
+    stu = student.objects.get(id=pk)
     context = {}
     userform = UserForm(instance=stu.user)
     sform = StudentForm(instance=stu, courseSet=course)
-    if request.method=='POST':
+    if request.method == 'POST':
         userform = UserForm(request.POST, instance=stu.user)
         sform = StudentForm(request.POST, instance=stu, courseSet=course)
-        try: 
+        try:
             if userform.is_valid():
                 userform.save()
                 if sform.is_vaild():
@@ -88,10 +93,12 @@ def update(request, pk):
                     return redirect('')
         except Exception as e:
             pass
-            
-    context = { 'userform': userform,
-                'sform': sform, 
-                'type' : 'Update', 
-                'id': stu.id}
 
-    return render(request, 'student/studentRegister.html ' , context)
+    context = {'userform': userform,
+               'sform': sform,
+               'type': 'Update',
+               'id': stu.id,
+               'title': 'Students'
+               }
+
+    return render(request, 'student/studentRegister.html ', context)
