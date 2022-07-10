e1c88cae7e28420ed9d04d802f09f2649921bad0
sd8capricon <sd8capricon@gmail.com>
2022-07-10 16:23:25 +0530

    ui to create time table

--- timetable/urls.py
+++ timetable/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('CreateTimeTable', views.createTT, name='createTimeTable'),
+    path('test', views.test_C),
     path('EditTimeTable', views.editTT, name='editTimeTable'),
     path('UpdateTimeTable', views.updateTT, name='updateTimeTable'),
     path('Requests', views.requests, name='requests'),
