e482b216d4b8c86b03d513164e099bb2c9f444b4
Sahil <saahil.shelke@gmail.com>
2022-07-12 20:20:26 +0530

    attendance2

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,36 @@
+from datetime import timedelta, datetime
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.http import HttpResponseRedirect
+from django.urls import path
+
+
+from .models import Assign_cls, Attendance, AttendanceClass
+
+# Register your models here.
+class AssignAdmin(admin.ModelAdmin):
+    list_display = ('cls', 'course', 'teacher')
+    search_fields = ('cls', 'course', 'course__name', 'teacher__user__firstname', 'course__code')
+    ordering = ['cls',  'course__id']
+    raw_id_fields = ['cls', 'course', 'teacher']
+
+
+class AttendanceClassAdmin(admin.ModelAdmin):
+    list_display = ('assign', 'status')
+    ordering = ['assign']
+    
+
+    '''def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path(),
+        ]
+        return my_urls + urls'''
+
+
+admin.site.register(Assign_cls, AssignAdmin)
+admin.site.register(AttendanceClass, AttendanceClassAdmin)
+admin.site.register(Attendance)
 
 # Register your models here.
--- attendance/models.py
+++ attendance/models.py
@@ -1,7 +1,42 @@
 from django.db import models
+from cls.models import Class
+from course.models import Course
+from student.models import student
+from users.models import User, TeacherProfile
 #attendance models
 
 # Create your models here.
+class Assign_cls(models.Model):
+    cls=models.ForeignKey(Class, on_delete=models.CASCADE)
+    course=models.ForeignKey(Course, on_delete=models.CASCADE )
+    teacher= models.ForeignKey(TeacherProfile, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('cls', 'course', 'teacher'),)
+
+    def __str__(self):
+        cl = Class.objects.get(id=self.cls_id)
+        cr = Course.objects.get(id=self.course_id)
+        te = TeacherProfile.objects.get(id=self.teacher_id)
+        return '%s : %s : %s' % (te, cr.name, cl)
+
+class AttendanceClass(models.Model):
+    assign=models.ForeignKey(Assign_cls, on_delete=models.CASCADE)
+    status = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Attendance'
+        verbose_name_plural = 'Attendance'
+    
+
 class Attendance(models.Model):
-    pass
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=1)
+    student = models.ForeignKey(student, on_delete=models.CASCADE, default=1)
+    attendanceclass = models.ForeignKey(AttendanceClass, on_delete=models.CASCADE, default=1)
+    status = models.BooleanField(default='True')
+
+    def __str__(self):
+        sname = student.objects.get(id=self.student_id)
+        cname = Course.objects.get(name=self.course)
+        return '%s : %s' % (sname.user.first_name, cname)
      
