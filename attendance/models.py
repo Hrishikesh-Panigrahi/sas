deced8a641564942e7cdcccba0194d74f848cfc8
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2022-07-13 05:46:30 +0530

    minor changes

--- attendance/models.py
+++ attendance/models.py
@@ -3,13 +3,15 @@ from cls.models import Class
 from course.models import Course
 from student.models import student
 from users.models import User, TeacherProfile
-#attendance models
+# attendance models
 
 # Create your models here.
+
+
 class Assign_cls(models.Model):
-    cls=models.ForeignKey(Class, on_delete=models.CASCADE)
-    course=models.ForeignKey(Course, on_delete=models.CASCADE )
-    teacher= models.ForeignKey(TeacherProfile, on_delete=models.CASCADE)
+    cls = models.ForeignKey(Class, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(TeacherProfile, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = (('cls', 'course', 'teacher'),)
@@ -20,23 +22,24 @@ class Assign_cls(models.Model):
         te = TeacherProfile.objects.get(id=self.teacher_id)
         return '%s : %s : %s' % (te, cr.name, cl)
 
+
 class AttendanceClass(models.Model):
-    assign=models.ForeignKey(Assign_cls, on_delete=models.CASCADE)
+    assign = models.ForeignKey(Assign_cls, on_delete=models.CASCADE)
     status = models.IntegerField(default=0)
 
     class Meta:
         verbose_name = 'Attendance'
         verbose_name_plural = 'Attendance'
-    
+
 
 class Attendance(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, default=1)
     student = models.ForeignKey(student, on_delete=models.CASCADE, default=1)
-    attendanceclass = models.ForeignKey(AttendanceClass, on_delete=models.CASCADE, default=1)
+    attendanceclass = models.ForeignKey(
+        AttendanceClass, on_delete=models.CASCADE, default=1)
     status = models.BooleanField(default='True')
 
     def __str__(self):
         sname = student.objects.get(id=self.student_id)
         cname = Course.objects.get(name=self.course)
         return '%s : %s' % (sname.user.first_name, cname)
-     
