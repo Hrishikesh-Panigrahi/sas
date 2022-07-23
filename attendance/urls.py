3996d1a58f5e41a74cf07203ebd30fba6b0cde38
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2022-07-23 08:59:34 +0530

    attendance

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<str:pk>/attendance/class', views.attendanceclass, name='attendance-class'),
-    path('<str:pk>/attendance/class/<str:co>', views.attendancecourse, name='attendance-course'),
-
+    # path('<str:pk>/attendance/class/<str:co>', views.attendancecourse, name='attendance-course'),
+    path('<str:pk>/attendance/class/<str:cls_id>/', views.attendance ,name='attendance')
 ]
