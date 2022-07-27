2bd686a00fda6928960503f76e995a228b12b4ab
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2022-07-27 20:22:54 +0530

    attendance

--- attendance/urls.py
+++ attendance/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('<str:pk>/attendance/class', views.attendanceclass, name='attendance-class'),
     path('attendance/class/<str:assign_id>/', views.attendance_date ,name='attendance-attendance_date'),
     path('attendance/<str:attendancecls_id>/', views.stu_list ,name='attendance'),
-    # path('attendance/<str:attendancecls_id>/confirm', views.confirm_attendance ,name='confirm-attendance'),
+    path('editattendance/<str:attendancecls_id>', views.edit_attendance ,name='edit-attendance'),
 ]
